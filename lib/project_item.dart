14a3c86074aa38d027c7d291ff415b37e475c8ca
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-04-20 21:18:48 -0300

    ProjectDetail agora eh um StatefullWidget pq o botao vai mudar

--- lib/project_item.dart
+++ lib/project_item.dart
@@ -73,7 +73,8 @@ class _ProjectItemState extends State<ProjectItem> {
   }
 }
 
-class ProjectDetail extends StatelessWidget {
+//tela dos detalhes de um projeto
+class ProjectDetail extends StatefulWidget {
   final String _name;
   final String _detail;
   final File _image;
@@ -81,10 +82,18 @@ class ProjectDetail extends StatelessWidget {
   ProjectDetail(this._name, this._detail, this._image);
 
   @override
+  _ProjectDetailState createState() => new _ProjectDetailState();
+}
+
+class _ProjectDetailState extends State<ProjectDetail> {
+
+  final Set _projSaved = Set(); //conjunto de projetos salvos/inscritos
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_name),
+        title: Text(widget._name),
       ),
       body: ListView(
         children: <Widget>[
@@ -93,7 +102,7 @@ class ProjectDetail extends StatelessWidget {
               shape: BoxShape.rectangle,
               border: Border.all(),
             ),
-            child: _image == null
+            child: widget._image == null
                 ? Icon(
                     Icons.image,
                     size: 240,
@@ -105,14 +114,14 @@ class ProjectDetail extends StatelessWidget {
                         MaterialPageRoute(
                           builder: (context) => Container(
                                 child: PhotoView(
-                                  imageProvider: FileImage(_image),
+                                  imageProvider: FileImage(widget._image),
                                 ),
                               ),
                         ),
                       );
                     },
                     child: Image.file(
-                      _image,
+                      widget._image,
                       height: 240,
                       fit: BoxFit.fitWidth,
                     ),
@@ -121,28 +130,44 @@ class ProjectDetail extends StatelessWidget {
           Container(
             padding: EdgeInsets.fromLTRB(20, 20, 20, 40),
             child: Text(
-              _detail,
+              widget._detail,
               style: TextStyle(fontSize: 16.0),
             ),
           ),
           Center(
-            child: RaisedButton(
-              //botao para se inscrever no projeto
-              onPressed: () {},
-              color: Colors.blue,
-              textColor: Colors.white,
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                'INSCREVA-SE',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-            ),
+            child: _subscribeButton(),
           ),
         ],
       ),
     );
   }
+
+  //botao para inscrever-se no projeto
+  Widget _subscribeButton() {
+    final bool alreadySaved = _projSaved.contains(widget._name);
+
+    //salvar Projeto
+    _saveProj() {
+      if(alreadySaved) {
+        _projSaved.remove(widget._name); //remove o projeto dos inscritos
+        //TODO: muda o botao para inscreva-se
+      } else {
+        _projSaved.add(widget._name); //adiciona o projeto aos inscritos
+        //TODO: muda o botao para inscrito
+      }
+    }
+
+    return RaisedButton(
+      //botao para se inscrever no projeto
+      onPressed: _saveProj, //vai para a funcao de salvar o projeto
+      color: Colors.blue,
+      textColor: Colors.white,
+      padding: const EdgeInsets.all(15.0),
+      child: Text(
+        'INSCREVA-SE',
+        style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  } //_subscribeButton
+
 }
