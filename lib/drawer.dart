4978d3843bbbb4e597ffb4d3be18c9bb49a1d8f4
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-05-13 16:00:37 -0300

    sidemenu mostra a quantidade de projetos inscritos do user

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -5,12 +5,12 @@ import 'package:scoped_model/scoped_model.dart';
 import 'config.dart';
 import 'models.dart';
 
-class Item{
+class Item {
   String nome;
   Item(this.nome);
 }
 
-class SideMenu extends StatefulWidget{
+class SideMenu extends StatefulWidget {
   final items = [
     new Item("Perfil"),
     new Item("Configurações"),
@@ -23,62 +23,72 @@ class SideMenu extends StatefulWidget{
   }
 }
 
-class _SideMenuState extends State<SideMenu>{
+class _SideMenuState extends State<SideMenu> {
   int _selectedDrawerIndex = 0;
-    changePages(int index){
-      switch(index){
-        case 1:
-          Navigator.push(context, MaterialPageRoute(builder: (context) => ConfigPage()));
-          break;
-        case 2:
-          FirebaseAuth.instance.signOut();
-          //widget.auth.signOut();
-          Navigator.pushReplacement(context, new MaterialPageRoute(builder: (context) => RootPage()));
-      }
-      setState((){
-        _selectedDrawerIndex = index;
-      });
+  changePages(int index) {
+    switch (index) {
+      case 1:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => ConfigPage()));
+        break;
+      case 2:
+        FirebaseAuth.instance.signOut();
+        //widget.auth.signOut();
+        Navigator.pushReplacement(
+            context, new MaterialPageRoute(builder: (context) => RootPage()));
     }
+    setState(() {
+      _selectedDrawerIndex = index;
+    });
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      var drawerOptions = <Widget>[];
-      for (var i = 0; i < widget.items.length; i++) {
-        var d = widget.items[i];
-        drawerOptions.add(
-            new ListTile(
-              title: new Text(d.nome),
-              selected: i == _selectedDrawerIndex,
-              onTap: () => changePages(i),
-            )
-        );
-      }
-      return ScopedModelDescendant<UserModel>(
-        builder: (context, child, user) =>  Drawer(
-          child: ListView( 
-            children: <Widget>[
-              DrawerHeader(
+  @override
+  Widget build(BuildContext context) {
+    var drawerOptions = <Widget>[];
+    for (var i = 0; i < widget.items.length; i++) {
+      var d = widget.items[i];
+      drawerOptions.add(new ListTile(
+        title: new Text(d.nome),
+        selected: i == _selectedDrawerIndex,
+        onTap: () => changePages(i),
+      ));
+    }
+    return ScopedModelDescendant<UserModel>(
+        builder: (context, child, user) => Drawer(
+              child: ListView(children: <Widget>[
+                DrawerHeader(
                   margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
                   child: Column(
                     children: <Widget>[
                       CircleAvatar(),
                       Material(
-                        textStyle: TextStyle(
-                          fontSize: 25,
-                          color: Colors.deepPurple
-                        ),
+                        textStyle:
+                            TextStyle(fontSize: 25, color: Colors.deepPurple),
                         child: Padding(
-                          padding: EdgeInsets.all(30.0),
-                          child: Text(user.username),
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text(user.username),
+                              Padding(padding: EdgeInsets.only(bottom: 8.0)),
+                              Text.rich(
+                                TextSpan(
+                                  text: user.userProjects.length.toString(), style: TextStyle(fontSize: 14),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: ' projetos inscritos',
+                                        style: TextStyle(fontSize: 15)),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       )
                     ],
-                  ),),
-              Column(children: drawerOptions)
-            ]
-          ),
-        )
-      );
-    }
+                  ),
+                ),
+                Column(children: drawerOptions)
+              ]),
+            ));
+  }
 }
-
