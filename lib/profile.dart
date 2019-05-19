b557f4e835be8459ec2c33ecfabff63d183520ce
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-05-19 17:02:26 -0300

    espaco para nome e foto apenas estilo

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Perfil'),
       ),
-      body: Center(
-        child: Text('test'),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(40.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    'Nome',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 20.0),
+                  ) 
+                ),
+                Expanded(
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20.0),
+                      child: CircleAvatar(),
+                    ),
+                  ),  
+                ),
+                
+              ],
+            ),
+          ),
+        ],
       ),
-      
     );
   }
 }
