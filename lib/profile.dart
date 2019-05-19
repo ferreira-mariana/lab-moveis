d7e6b8a5d90e621698c844fce672b57f6175f66c
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-05-19 19:11:15 -0300

    email e bio

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,41 +1,103 @@
 import 'package:flutter/material.dart';
+import 'models.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class ProfilePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Perfil'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(40.0),
-            child: Row(
+    return ScopedModelDescendant<UserModel>(
+      builder: (context, child, user) => Scaffold(
+            appBar: AppBar(
+              title: Text('Perfil'),
+            ),
+            body: Column(
               children: <Widget>[
-                Expanded(
-                  child: Text(
-                    'Nome',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontSize: 20.0),
-                  ) 
+                Padding(
+                  padding: EdgeInsets.all(40.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: Text(
+                        'Nome',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontSize: 20.0),
+                      )),
+                      Expanded(
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: CircleAvatar(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                Expanded(
-                  child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20.0),
-                      child: CircleAvatar(),
-                    ),
-                  ),  
+                Padding(
+                  padding: EdgeInsets.all(40.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            'Infos',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          RichText(
+                          text: TextSpan(
+                            text: 'Email ',
+                            style: TextStyle(color: Colors.grey[500], fontSize: 16),
+                            children: <TextSpan>[
+                             TextSpan(
+                                 text: user.username,
+                                 style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey[700]) 
+                              ),
+                             ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          RichText(
+                          text: TextSpan(
+                            text: 'Bio ',
+                            style: TextStyle(color: Colors.grey[500], fontSize: 16),
+                            children: <TextSpan>[
+                             TextSpan(
+                                 text: "olá, eu sou fulana",
+                                 style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey[700])),
+                             ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      // RichText(
+                      //   text: TextSpan(
+                      //     text: 'Email ',
+                      //     style: TextStyle(color: Colors.grey[700]),
+                      //     children: <TextSpan>[
+                      //       TextSpan(
+                      //           text: user.username,
+                      //           style: TextStyle(fontWeight: FontWeight.bold)),
+                      //     ],
+                      //   ),
+                      // ),
+                    ],
+                  ),
                 ),
-                
               ],
             ),
           ),
-        ],
-      ),
     );
   }
 }
