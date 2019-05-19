d2f640923749f4ed527e83bc0607cb1b18dc27d2
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-05-19 19:39:54 -0300

    refatorando perfil

--- lib/profile.dart
+++ lib/profile.dart
@@ -7,101 +7,69 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModelDescendant<UserModel>(
       builder: (context, child, user) => Scaffold(
-            appBar: AppBar(
-              title: Text('Perfil'),
-            ),
-            body: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(40.0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          child: Text(
-                        'Nome',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontSize: 20.0),
-                      )),
-                      Expanded(
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 20.0),
-                            child: CircleAvatar(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(40.0),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
+          appBar: AppBar(
+            title: Text('Perfil'),
+          ),
+          body: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
                         children: <Widget>[
                           Text(
-                            'Infos',
+                            'Nome',
                             textAlign: TextAlign.left,
-                            style: TextStyle(
-                                color: Colors.purple,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 18),
+                            style: TextStyle(fontSize: 20.0),
                           ),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
                           RichText(
-                          text: TextSpan(
-                            text: 'Email ',
-                            style: TextStyle(color: Colors.grey[500], fontSize: 16),
-                            children: <TextSpan>[
-                             TextSpan(
-                                 text: user.username,
-                                 style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey[700]) 
-                              ),
-                             ],
+                            text: TextSpan(
+                              text: user.userProjects.length.toString(),
+                              style: TextStyle(
+                                  color: Colors.grey[700], fontSize: 16),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: ' projetos inscritos',
+                                    style: TextStyle(color: Colors.grey[700])),
+                              ],
                             ),
                           ),
                         ],
                       ),
-                      Row(
-                        children: <Widget>[
-                          RichText(
-                          text: TextSpan(
-                            text: 'Bio ',
-                            style: TextStyle(color: Colors.grey[500], fontSize: 16),
-                            children: <TextSpan>[
-                             TextSpan(
-                                 text: "olá, eu sou fulana",
-                                 style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey[700])),
-                             ],
-                            ),
-                          ),
-                        ],
+                    ),
+                    Expanded(
+                      child: FittedBox(
+                        fit: BoxFit.contain,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20.0),
+                          child: CircleAvatar(),
+                        ),
                       ),
-                      Row(
-                        children: <Widget>[
-                          RichText(
-                          text: TextSpan(
-                            text: 'Projetos inscritos ',
-                            style: TextStyle(color: Colors.grey[500], fontSize: 16),
-                            children: <TextSpan>[
-                             TextSpan(
-                                 text: user.userProjects.length.toString(),
-                                 style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey[700])),
-                             ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
+              ListTile(
+                title: Text(
+                  'Infos',
+                  style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18),
+                ),
+              ),
+              ListTile(
+                  leading:
+                      Text('Email', style: TextStyle(color: Colors.grey[600])),
+                  title: Text(user.username)),
+              ListTile(
+                  leading:
+                      Text('Bio', style: TextStyle(color: Colors.grey[600])),
+                  title: Text('ola, eu sou fulana')),
+            ],
+          )),
     );
   }
 }
